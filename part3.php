01bf9ba7393b53f4cc40140dbb842601a7a0c9ca
B00151316 Sadia Hossain <B00151316@mytudublin.ie>
2024-10-09 09:53:39 +0100

    part3.php completed

---
+++ part3.php
@@ -0,0 +1,38 @@
+<?php
+//Give the name of the program here
+//Include your name and the date here
+//Give a brief description of what the program does
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tennisclub";
+$port = 3307;
+
+//set the default timezone - this is necessary since MySQL 8. This is an effort to store all dates and times together with their timezones. 
+//This is particularly important where there is a timestamp indicating when something happened.
+date_default_timezone_set('Europe/Dublin');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstname, surname FROM Member";
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo " Member Name: " . $row["firstname"] . " " . $row["surname"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
